34324f136dbc0971ac2fb3bd92ecf185f6c4ca84
drftgyhuygt <gs1hyperder@hotmail.com>
2024-05-09 01:24:56 +0700

    demo commit

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-function combinationSum4(nums, target) {
-  const dp = new Array(target + 1).fill(0);
+function numDecodings(s) {
+  const dp = new Array(s.length + 1).fill(0);
   dp[0] = 1;
-  for (let i = 1; i <= target; i++) {
-    for (const num of nums) {
-      if (i >= num) {
-        dp[i] += dp[i - num];
-      }
+  dp[1] = s[0] === "0" ? 0 : 1;
+  for (let i = 2; i <= s.length; i++) {
+    const oneDigit = parseInt(s.substring(i - 1, i));
+    const twoDigits = parseInt(s.substring(i - 2, i));
+    if (oneDigit >= 1) {
+      dp[i] += dp[i - 1];
+    }
+    if (twoDigits >= 10 && twoDigits <= 26) {
+      dp[i] += dp[i - 2];
     }
   }
-  return dp[target];
+  return dp[s.length];
 }
