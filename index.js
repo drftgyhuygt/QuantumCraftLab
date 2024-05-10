dd2e7a92912ccdd63a287cb8a94bb59e35a9bdd0
drftgyhuygt <gs1hyperder@hotmail.com>
2024-05-10 23:32:30 +0700

    update aptos

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-function subsetsWithDup(nums) {
-  const result = [];
-  nums.sort((a, b) => a - b);
-  backtrack(0, []);
-  return result;
-  function backtrack(start, current) {
-    result.push([...current]);
-    for (let i = start; i < nums.length; i++) {
-      if (i > start && nums[i] === nums[i - 1]) continue;
-      current.push(nums[i]);
-      backtrack(i + 1, current);
-      current.pop();
-    }
-  }
-}
+const shuffle = (arr) => arr.sort(() => Math.random() - 0.5);
