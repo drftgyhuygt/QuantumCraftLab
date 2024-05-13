81caa0e1bccedbf0fb5d62487ad0a28d907b0cdb
drftgyhuygt <gs1hyperder@hotmail.com>
2024-05-13 09:07:46 +0700

    test

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const lcm = (a, b) => {
-  return (a * b) / gcd(a, b);
+const insertionSortRecursive = (arr, n = arr.length) => {
+  if (n <= 1) {
+    return arr;
+  }
+  insertionSortRecursive(arr, n - 1);
+  const last = arr[n - 1];
+  let j = n - 2;
+  while (j >= 0 && arr[j] > last) {
+    arr[j + 1] = arr[j];
+    j--;
+  }
+  arr[j + 1] = last;
+  return arr;
 };
